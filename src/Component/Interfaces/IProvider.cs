ebf530d6650ea6726b53989faf60d98c4a0a41a3
andyvernon <andy_vernon@live.co.uk>
2023-06-19 22:15:18 +0100

    Updates

---
+++ src/Component/Interfaces/IProvider.cs
@@ -0,0 +1,9 @@
+﻿using MoviesAndTV.Components.GoT.Domain;
+
+namespace MoviesAndTV.Components.GoT.Interfaces
+{
+    public interface IProvider
+    {
+        Task<Character> GetCharacter(string name);
+    }
+}
---
+++ src/MoviesAndTV.Components.GoT.Domain/Character.cs
@@ -0,0 +1,8 @@
+﻿namespace MoviesAndTV.Components.GoT.Domain
+{
+    public class Character
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
